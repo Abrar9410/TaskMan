d5ca2a8de28cae3b8b2bf8659fc6b063627e7ac6
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-02-21 00:13:52 +0600

    First commit with Firebase setup completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl'>TASK MANAGER</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable react/prop-types */
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.config";
+import { axiosPublic } from "../utilities/utilities";
+import { AuthContext } from "../AuthContext";
+
+
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches); //Temporary- For this app only
+
+    // Google Sign-In
+    const googleProvider = new GoogleAuthProvider();
+    const loginWithGoogle = () => {
+        setLoading(true);
+        return (
+            signInWithPopup(auth, googleProvider)
+            // .then(result => setUser(result.user))
+            // .catch(error => alert("ERROR", error.code))
+        );
+    }
+    
+
+    // Update User-Profile
+    const updateUserProfile = (updateInfo) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, updateInfo);
+        // .then(() => {
+            //     setLoading(false);
+            //     navigate("/user-profile");
+            // })         
+        // .catch(error => setErrorMessage(error.message));
+    }
+
+
+    // Log-Out 
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async currentUser => {
+            setUser(currentUser);
+            if (currentUser?.email) {
+                setLoading(false);
+            }
+            else {
+                setLoading(false);
+            }
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        setUser,
+        loginWithGoogle,
+        loading,
+        setLoading,
+        updateUserProfile,
+        logOut,
+        isDarkMode, setIsDarkMode,   //Temporary- For this app only
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
